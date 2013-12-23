5d49ed5787655d7d5306d49b812ee153e7234b6f
histand <histand@gmail.com>
2013-12-23 13:55:30 -0800

    python file to acquire time column

---
+++ column_extractor2time.py
@@ -0,0 +1,38 @@
+# Miles Histand
+
+# This program will pull the nth column from a comma delimited table  (CSV) and
+# save to an output file
+
+
+import sys
+import re
+import os
+import csv
+
+def main():
+    arg = 0        # column to extract
+    w = open('/Users/histand/Desktop/outfile_time.txt','w')
+
+    for csvFiles in os.listdir("/Users/histand/Desktop/Test_Data"):
+        
+        argument = 0                    # set to 0 to remove header during first iteration
+        
+        if csvFiles.startswith("."):
+            continue
+        
+        reader = csv.reader(open('/Users/histand/Desktop/Test_Data/'+csvFiles,'r'))
+        for row in reader:
+            if argument > 1:
+                column = row[arg]                               # desired column to store   
+                column = column.strip()                       # remove any \n or stuff
+                print csvFiles + column
+                w.write(column)                           # write out to file
+                w.write('\n')
+            argument = argument + 1    
+   
+
+    w.close()                                         # close written file
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
