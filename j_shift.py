76266d04331466762923f0d81ec2b7605c36640d
histand <histand@gmail.com>
2014-02-17 07:09:25 -0800

    update j_shifting file

--- j_shift.py
+++ j_shift.py
@@ -1,23 +1,46 @@
 # Miles Histand
 
-# This program will search for the username of a specific site entered by user
+# This program will pull the nth column from a comma delimited table  (CSV) and
+# save to an output file
 
 
 import sys
 import re
+import os
+import csv
 
 def main():
-    x = open('/Users/histand/Desktop/Data.txt','r')
-    y = x.readline()
-    # z= list(y)
-    print y
-    f=y.strip('j')
-    print f
-    z=list(f)
-    z.append('j')
-    z = "".join(z)
-    print z
-    
+    count=0
+    arg = 1        # column to extract
+    w = open('/Users/histand/Desktop/outfile.csv','w')
+
+    for csvFiles in os.listdir("/Users/histand/Desktop/Data"):
+        
+     
+        
+        if csvFiles.startswith("."):
+            continue
+        
+        reader = csv.reader(open('/Users/histand/Desktop/Data/'+csvFiles,'r'))
+        for row in reader:
+            
+                count=count+1
+                column = row[arg]                               # desired column to store   
+                #column = column.strip()                       # remove any \n or stuff
+                                                    #print csvFiles + column
+                                                    #f = column.strip('j')
+                f = column.translate(None,'j')
+                print f
+                z = list(f)
+                z.append('j')
+                z = "".join(z)
+                w.write(z)                           # write out to file
+                w.write('\n')
+                
+    print count
+
+    w.close()                                         # close written file
+
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
   main()
